32c25b2a5a61bf84952aedd53d634f2ae0d2c80c
DaWaS1 <91870739+DaWaS1@users.noreply.github.com>
2021-11-16 13:03:44 +0100

    Update

--- src/com/codebin/Form.java
+++ src/com/codebin/Form.java
@@ -13,6 +13,11 @@ public class Form {
     private JLabel JL_km;
     private JLabel LF_fuzany;
     private JPanel panelMain;
+    private JComboBox JC_combobox;
+    private JLabel JL_ar;
+    private JTextField Jtxt_ar;
+    private JLabel JL_forint;
+    private JLabel JL_koltseg;
 
     public Form() {
         Jb_feldolgoz.addActionListener(new ActionListener() {
@@ -20,8 +25,23 @@ public class Form {
             public void actionPerformed(ActionEvent e) {
                 double fogyasztas = Double.parseDouble(Jtxt_fogyaszta.getText());
                 double km = Double.parseDouble(Jtxt_km.getText());
-                double felhasznaltuzi = fogyasztas * km;
+                double felhasznaltuzi = fogyasztas/100 * km;
                 Jtxt_felhasznaltuz.setText(Double.toString(felhasznaltuzi));
+            }
+        });
+        JC_combobox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = JC_combobox.getSelectedIndex();
+                String uza = JC_combobox.getSelectedItem() + "";
+                switch (index)
+                {
+                    case 0 : Jtxt_ar.setText("460"); break;
+                    case 1 : Jtxt_ar.setText("480"); break;
+                    case 3 : Jtxt_ar.setText("490"); break;
+                    default: Jtxt_ar.setText("470");
+                }
+
             }
         });
     }
